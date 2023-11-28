b5861ec879ac04fab2c7df385516600650cf8bd4
Cândido Júnior <candido101209@gmail.com>
2023-11-28 00:21:54 -0300

    added path validation and removed magic variables

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import pyautogui
+from pathlib import Path
 
-def auto_typer(code_path, interval):
-    with open(code_path, "r") as f:
-        text = f.read().splitlines()
-    
+INDENTATION_WIDTH = 4
+
+def auto_typer(file_path, interval):
+    if not Path(file_path).is_file():
+        raise FileNotFoundError(f"The file '{file_path}' does not exist.")
+
+    with open(file_path, "r") as file:
+        lines = file.read().splitlines()
+
     pyautogui.sleep(interval)
 
     prev_spaces = 0
     prev_line = ""
-    for line in text:
+
+    for line in lines:
         spaces = 0
-        if line == "":
+
+        if not line:
             pyautogui.press('enter')
         else:
-            while line.startswith("    "):
+            while line.startswith(" " * INDENTATION_WIDTH):
                 spaces += 1
-                line = line[4:]
+                line = line[INDENTATION_WIDTH:]
 
             auto_tabs = 1 if prev_line.endswith(":") else 0
-            auto_backspaces = 1 if prev_line.startswith("return") else 0
+            auto_backspaces = 1 if prev_line.startswith(("return", "raise")) else 0
+
 
             delta_spaces = spaces - prev_spaces
             delta_auto_spaces = auto_tabs - auto_backspaces
@@ -27,7 +36,7 @@ def auto_typer(code_path, interval):
 
             if current_change > 0:
                 pyautogui.press('tab', current_change)
-            if current_change < 0:
+            elif current_change < 0:
                 pyautogui.press('backspace', abs(current_change))
 
             pyautogui.write(line, interval=0.05)
